7eeaf6262205f4b6126dced38e474949cc42fb4d
Vicki Langer <vicki.langer@statalog.org>
2019-04-14 23:16:54 -0400

    Updated mail.php - contact email added and sent/fail messages changed

--- mail.php
+++ mail.php
@@ -1,9 +1,9 @@
 <?php
-    $to = 'spn5@spondonit.com';
+    $to = 'Hello@Statalog.org';
     $firstname = $_POST["fname"];
     $email= $_POST["email"];
     $text= $_POST["message"];
-    
+
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
@@ -16,14 +16,14 @@
         </tr>
         <tr><td>Email: '.$email.'</td></tr>
         <tr><td>Email: '.$text.'</td></tr>
-        
+
     </table>';
 
     if (@mail($to, $email, $message, $headers))
     {
-        echo 'The message has been sent.';
+        echo "Your message is making it's way to us!;
     }else{
-        echo 'failed';
+        echo 'Oops, it seems something is wrong. Please, try again';
     }
 
 ?>
